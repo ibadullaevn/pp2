97649e6e6beeeff561234fc28ca37901cf478186
ibadullaevn <40333337+ibadullaevn@users.noreply.github.com>
2019-02-14 02:17:41 +0600

    Add files via upload

---
+++ Week 2/Task3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void f(DirectoryInfo dir, int j)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            string s = "";
+            for (int i = 0; i < j; i++)
+            {
+                s += " ";
+            }
+            Console.WriteLine(s + dir.Name);
+            FileSystemInfo[] x = dir.GetFileSystemInfos();
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i].GetType() == typeof(DirectoryInfo))
+                {
+                    //Console.WriteLine("YES");
+                    DirectoryInfo dir2 = x[i] as DirectoryInfo;
+                    f(dir2, j + 4);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(s + x[i]);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo dirinfo = new DirectoryInfo(@"C:\Users\Asus\Desktop\1");
+            f(dirinfo, 0);
+        }
+    }
+}
