e91e2380c8b7d206097bdee1bef373e0989b0d8a
ibadullaevn <40333337+ibadullaevn@users.noreply.github.com>
2019-02-13 17:26:57 +0600

    Add files via upload
    Fc

---
+++ pp2/Week1/Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task
+{
+    public class student
+        
+    {
+        string name;
+        string id;
+        int year;
+        public student(string NAME, string ID, int Year)
+        {
+            name = NAME;
+            id = ID;
+            year = Year;
+        }
+        public void output()
+        {
+            Console.Write(name + " " + id + " " + " " + year);
+        }
+        public int increment()
+        {
+            return year++;
+
+        }
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string i = Console.ReadLine();
+            int y = int.Parse(Console.ReadLine());
+            student s1 = new student(s, i, y);
+            s1.increment();
+            s1.output();
+        }
+    }
+}
